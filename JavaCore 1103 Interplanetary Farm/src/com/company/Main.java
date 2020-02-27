6cdd54b0a1884e182ad47418a1ce8da579e14fac
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-27 11:18:58 +0200

    Add files via upload

---
+++ JavaCore 1103 Interplanetary Farm/src/com/company/Main.java
@@ -0,0 +1,79 @@
+package com.company;
+
+/*
+
+1103 Interplanetary Farm
+Write six classes: Animal (animal), Cow (cow), Pig (pig), Sheep (sheep), Bull (bull) and Goat (goat).
+Inherit the cow, pig, sheep, bull and goat from the animal.
+
+Requirements:
+1. In the class Solution there must be a public class Animal (animal).
+2. In the class Solution there must be a public class Cow (cow).
+3. In the class Solution there must be a public class Pig (pig).
+4. In the class Solution there must be a public class Sheep (sheep).
+5. In the class Solution there must be a public class Bull (bull).
+6. In the class Solution there must be a public class Goat (goat).
+7. The Cow class must be inherited from the Animal class.
+8. The Pig class must be inherited from the Animal class.
+9. The Sheep class must be inherited from the Animal class.
+10. The Bull class must be inherited from the Animal class.
+11. The Goat class must be inherited from the Animal class.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {}
+    public class Animal {
+
+    }
+
+    public class Cow extends Animal  {
+
+    }
+
+    public class Pig extends Animal  {
+
+    }
+
+    public class Sheep extends Animal {
+
+    }
+
+    public class Bull extends Animal {
+
+    }
+
+    public class Goat extends Animal {
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
